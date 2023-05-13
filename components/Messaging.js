de380ec3014012b98de890fcf841bec9ac037954
alreemnasser <133537704+alreemnasser@users.noreply.github.com>
2023-05-14 00:55:34 +0400

    Update Messaging.js

--- components/Messaging.js
+++ components/Messaging.js
@@ -6,7 +6,7 @@ export default function Messaging(){
     return(
         <View style={{marginBottom:20,borderWidth:1,marginHorizontal:10, width:'95%', borderRadius:20}}>
 
-            <TextInput style={{margin:20}} placeholder="start chatting with Alli"    value ={value}/>
+            <TextInput style={{margin:20}} placeholder="start chatting with Alli"    value ={value}/
             
         </View>
     )
